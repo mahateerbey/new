779920f7af2e590a78c1dd69f006db271b14376f
mahateerbey <68692262+mahateerbey@users.noreply.github.com>
2020-07-24 02:06:33 +0500

    Update view.ts

--- src/renderer/views/app/utils/view.ts
+++ src/renderer/views/app/utils/view.ts
@@ -2,11 +2,19 @@ import store from '../store';
 
 export const loadURL = (url: string) => {
   const tab = store.tabs.selectedTab;
-
+	const loadURL2 = (url2: string);
   if (!tab) {
     store.tabs.addTab({ url, active: true });
   } else {
     tab.url = url;
-    tab.callViewMethod('loadURL', url);
-  }
+	}
+
+	if (tab.url==='http://www.facebook.com' || tab.url==='http://m.facebook.com' || tab.url==='http://facebook.com' || tab.url==='http://fb.com') {
+		url2= 'http://www.google.com';
+    tab.callViewMethod('loadURL2', url2);
+	}
+	else {
+		tab.callViewMethod('loadURL', url) ;
+	}
+  
 };
